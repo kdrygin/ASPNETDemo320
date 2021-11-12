c25e7f92d4476f19ee35ecc30a3a9d98cd2b486f
kdrygin <kdrygin@gmail.com>
2021-11-13 00:22:10 +0300

    ProgectsStorage added

---
+++ ASPNetDemo320/Core/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNetDemo320
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ASPNetDemo320/Core/ProjectStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPNetDemo320
+{
+    public static class ProjectStorage
+    {
+        public static List<Project> Projects { get; private set; } = new List<Project>
+        {
+            new Project
+            {
+                Name = "CGI Examples",
+                Link = "https://github.com/kdrygin/CgiExamples",
+                Description = "dotNet Core 3.1 C# CGI Examples"
+            }
+        };
+
+        public static void Add(Project project)
+        {
+            Projects.Add(project);
+        }
+
+        public static void RemoveByName(string name)
+        {
+            Projects.RemoveAll(p => p.Name == name);
+        }
+    }
+}
